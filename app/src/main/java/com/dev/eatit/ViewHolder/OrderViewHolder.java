75e7fd441a61b54a87016ddc5b56fb72c9a95f4e
ShinMyeongJi <mj.swink95@gmail.com>
2020-03-17 23:11:15 +0900

    주문 내역 오류 해결

--- app/src/main/java/com/dev/eatit/ViewHolder/OrderViewHolder.java
+++ app/src/main/java/com/dev/eatit/ViewHolder/OrderViewHolder.java
@@ -11,7 +11,7 @@ import com.dev.eatit.R;
 
 public class OrderViewHolder extends RecyclerView.ViewHolder implements View.OnClickListener {
 
-    private TextView order_id, order_status, order_phone, order_address;
+    public TextView order_id, order_status, order_phone, order_address;
 
     private ItemClickListener itemClickListener;
 
@@ -34,35 +34,5 @@ public class OrderViewHolder extends RecyclerView.ViewHolder implements View.OnC
         itemClickListener.onClick(v, getAdapterPosition(), false);
     }
 
-    public TextView getOrder_id() {
-        return order_id;
-    }
 
-    public void setOrder_id(TextView order_id) {
-        this.order_id = order_id;
-    }
-
-    public TextView getOrder_status() {
-        return order_status;
-    }
-
-    public void setOrder_status(TextView order_status) {
-        this.order_status = order_status;
-    }
-
-    public TextView getOrder_phone() {
-        return order_phone;
-    }
-
-    public void setOrder_phone(TextView order_phone) {
-        this.order_phone = order_phone;
-    }
-
-    public TextView getOrder_address() {
-        return order_address;
-    }
-
-    public void setOrder_address(TextView order_address) {
-        this.order_address = order_address;
-    }
 }
